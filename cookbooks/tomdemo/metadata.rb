1c765993775896594a7745a1367fb1ecce6d70c7
thomas-stern <thomas@rightscale.com>
2012-09-24 20:48:05 +0000

    Added a hello world recipe

--- cookbooks/tomdemo/metadata.rb
+++ cookbooks/tomdemo/metadata.rb
@@ -1,6 +1,9 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+
+maintainer       "Tom Stern"
+maintainer_email "thomas@rightscale.comL"
 license          "All rights reserved"
 description      "Installs/Configures tomdemo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe "tomdemo::helloworld", "Prints hello world"
